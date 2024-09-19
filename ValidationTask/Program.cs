48c372a5f4c64f1031bf1242cdbdc8d251b18cc1
Alice Hulbert <21AHulbert@heckgrammar.co.uk>
2024-09-19 10:00:26 +0100

    this might be going hiorribly

--- ValidationTask/Program.cs
+++ ValidationTask/Program.cs
@@ -22,20 +22,20 @@
 
 
 
-            username = createUserName(firstName,lastName,age);
+            username = createUserName(firstName, lastName, age);
             Console.WriteLine($"Username is {username}, you have successfully registered please remember your password");
 
             //  Test your program with a range of tests to show all validation works
             // Show your evidence in the Readme
 
         }
-        static bool ValidName(string name, string firstName, string lastName) //i think im done
+        static bool ValidName(string name) //i think im done
         {
-            name = firstName + lastName;
+            //name = firstName + lastName;
             int nameLength = name.Length;
             while (nameLength >= 2)  //name must be at least two characters
             {
-                if (name.All(char.IsLetter))  //contain only letters
+                while (name.All(char.IsLetter))  //contain only letters
                 {
                     return true;
                 }
@@ -53,7 +53,7 @@
             return false;
         }
 
-   
+
         static bool ValidPassword(string password)
         {
             int passwordLength = password.Length;
@@ -62,14 +62,14 @@
             bool nonLettersCheck = password.All(char.IsLetter);
             while (passwordLength >= 8)  // Check password is at least 8 characters in length //done
             {
-                if (lowerCaseCheck == true && upperCaseCheck == true && nonLettersCheck == false)  // QWErty%^& = validCheck password contains a mix of lower case, upper case and non letter characters //done
-                                                                                                   // QWERTYUi = not valid
-                                                                                                   // ab£$%^&* = not valid
-                                                                                                   // QWERTYu! = valid
+                while (lowerCaseCheck == true && upperCaseCheck == true && nonLettersCheck == false)  // QWErty%^& = validCheck password contains a mix of lower case, upper case and non letter characters //done
+                                                                                                      // QWERTYUi = not valid
+                                                                                                      // ab£$%^&* = not valid
+                                                                                                      // QWERTYu! = valid
                 {
-                    if
+                    // while()
                     {
-                        // Check password contains no runs of more than 2 consecutive or repeating letters or numbers //COME BACK TO MEEEEe
+                        // Check password contains no runs of more than 2 consecutive or repeating letters or numbers //COME BACK TO MEEEE
                         // AAbbdd!2 = valid (only 2 consecutive letters A and B and only 2 repeating of each)
                         // abC461*+ = not valid (abC are 3 consecutive letters)
                         // 987poiq! = not valid (987 are consecutive)
@@ -83,35 +83,52 @@
                         //    if (s[i] - s[i - 1] != 1)
                         //        return false;
                         //}
+                    }
                 }
+
+
+
+            }
+            static bool validEmail(string email)  //come back to meeee
+            {
+                // a valid email address
+                // has at least 2 characters followed by an @ symbol
+                // has at least 2 characters followed by a .
+                // has at least 2 characters after the .
+                // contains only one @ and any number of .
+                // does not contain any other non letter or number characters
+                string shortenedEmail1 = email.Substring(2, email.Length - 1);
+                char searchKey = '@';
+                bool followedByAnAt;
+                int atIndex = (shortenedEmail1.IndexOf('@', 0, shortenedEmail1.Length));
+                string shortenedEmail2 = shortenedEmail1.Substring(atIndex, (email.Length - 1));
+                for (int i = 0; i < shortenedEmail1.Length; i++)
+                {
+                    if (shortenedEmail1[i] == searchKey)
+                    {
+                        followedByAnAt = true;
+                    }
+                }
+                while (email.Substring(0) != "@" && email.Substring(1, 1) != "@")
+                {
+                    while (followedByAnAt = true)
+                    {
+                        while (shortenedEmail2.Substring(0) != '.' && shortenedEmail2.Substring(1, 1)  '.' );
+                    
+                
+            static string createUserName(string firstName, string lastName, int age)  //DONEEEE
+            {
+                // username is made up from:
+                string firstTwoLetters = firstName.Substring(1);  // first two characters of first name
+                int lastNameLength = lastName.Length - 1;
+                string lastTwoLetters = lastName.Substring((lastNameLength - 1), lastNameLength);  // last two characters of last name
+                string userName = (firstTwoLetters + lastTwoLetters + age); // age
+                                                                            //e.g. Bob Smith aged 34 would have the username Both34
+                return userName;
+
+
             }
 
-
-
         }
-        static bool validEmail(string email)
-        {
-            // a valid email address
-            // has at least 2 characters followed by an @ symbol
-            // has at least 2 characters followed by a .
-            // has at least 2 characters after the .
-            // contains only one @ and any number of .
-            // does not contain any other non letter or number characters
-
-
-        }
-        static string createUserName(string firstName, string lastName, int age)  //DONEEEE
-        {
-            // username is made up from:
-            string firstTwoLetters = firstName.Substring(1);  // first two characters of first name
-            int lastNameLength = lastName.Length - 1;  
-            string lastTwoLetters = lastName.Substring((lastNameLength - 1), lastNameLength);  // last two characters of last name
-            string userName = (firstTwoLetters + lastTwoLetters + age); // age
-                                                                        //e.g. Bob Smith aged 34 would have the username Both34
-            return userName;
-
-
-        }
-
     }
 }
